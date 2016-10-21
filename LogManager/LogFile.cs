b7803736517db7113c91abd0e6db2443ae74d52f
林嘉豪 <1143910315@qq.com>
2016-10-21 09:16:49 +0800

    测试多线程

--- LogManager/LogFile.cs
+++ LogManager/LogFile.cs
@@ -1,9 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
+using System.Windows.Forms;
 
 namespace LogManager {
     static class LogFile {
-
+		static LogFile() {
+			/*
+			Thread th = new Thread(new ThreadStart(check));
+			th.IsBackground=true;	
+			th.Start();
+			MessageBox.Show("正在运行1");
+			*/
+			new Thread(new ThreadStart(check)).Start();
+			for (int i = 0; i < 100000;i++ ) {
+				Console.WriteLine("1");
+			}
+		}
+		static void check() {
+			for (int i = 0; i < 100000; i++) {
+				Console.WriteLine("2");
+			}
+		}
+		static void Main() {
+			MessageBox.Show("正在运行3");
+		}
     }
 }

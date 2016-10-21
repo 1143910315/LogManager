003c6d4623355c4e0c3cdac8b7c9f8da183bdf56
林嘉豪 <1143910315@qq.com>
2016-10-21 11:21:15 +0800

    尝试获取多线程句柄
    未实现

--- LogManager/LogFile.cs
+++ LogManager/LogFile.cs
@@ -3,9 +3,13 @@ using System.Collections.Generic;
 using System.Text;
 using System.Threading;
 using System.Windows.Forms;
+using System.Runtime.InteropServices;
 
 namespace LogManager {
     static class LogFile {
+		[DllImport("kernel32")]
+		private static extern int WaitForSingleObject(int hHandle, int dwMilliseconds);
+		private static int ThreadId;
 		static LogFile() {
 			/*
 			Thread th = new Thread(new ThreadStart(check));
@@ -13,18 +17,24 @@ namespace LogManager {
 			th.Start();
 			MessageBox.Show("正在运行1");
 			*/
+			ThreadId = Thread.CurrentThread.ManagedThreadId;
 			new Thread(new ThreadStart(check)).Start();
+
 			for (int i = 0; i < 100000;i++ ) {
-				Console.WriteLine("1");
+				Console.WriteLine(Thread.CurrentThread.ManagedThreadId.ToString());
 			}
 		}
-		static void check() {
-			for (int i = 0; i < 100000; i++) {
-				Console.WriteLine("2");
+		private static void check() {
+			while (true) {
+				if (0 == WaitForSingleObject(ThreadId, 100)) {
+					Console.WriteLine("安全退出");
+					return;
+				}
+				Console.WriteLine(Thread.CurrentThread.ManagedThreadId.ToString());
 			}
 		}
 		static void Main() {
-			MessageBox.Show("正在运行3");
+			MessageBox.Show(Thread.CurrentThread.ManagedThreadId.ToString());
 		}
     }
 }
